2bb9da43088697cc2d57a7bf9498c3c36f6ef393
Mira Thoen Feiring <mirafeiring@gmail.com>
2016-07-05 12:12:48 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+	entry: './index.js',
+
+	output: {
+		filename: 'bundle.js',
+		publicPath: ''
+	},
+
+	module: {
+		loaders: [{
+			test: /\.js?$/,
+			exclude: /node_modules/,
+			loader: 'babel-loader?presets[]=es2015&presets[]=react'
+		}]
+	}
+}
+// module.exports = {
+//   entry: './index.js',
+
+//   output: {
+//     filename: 'bundle.js',
+//     publicPath: ''
+//   },
+
+//   module: {
+//     loaders: [
+//       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
+//     ]
+//   }
+// }
